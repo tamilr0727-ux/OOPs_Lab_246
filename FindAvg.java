de4cfaa4c46b80c81d5751b1561282ee5c37e91f
tamilr0727-ux <tamilr0727@gmail.com>
2025-12-22 17:35:24 +0530

    Add files via upload

---
+++ FindAvg.java
@@ -0,0 +1,10 @@
+class FindAvg{
+	public static int findAvg(int numA,int numB,int numC){
+		return (numA+numB+numC)/3;
+	}
+	public static void main(String[] args){
+		FindAvg obj=new FindAvg();
+		int avg=obj.findAvg(10,20,30);
+		System.out.println("The Average of Three Numbers:" +avg);
+	}
+}
