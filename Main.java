4c7ee77a9ea7028e3ccce7a64c9b4c4d4c280e17
tamilr0727-ux <tamilr0727@gmail.com>
2025-12-22 17:43:46 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+	final int x=10;
+	final double PI=3.14;
+	public static void main(String[] args){
+		Main myObj=new Main();
+		System.out.println(myObj.PI);
+		System.out.println(myObj.x);
+	}
+}
