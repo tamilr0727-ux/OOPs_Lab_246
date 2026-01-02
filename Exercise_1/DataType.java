bb7eb24ab51ad71684a6ef657b848a1e55e493bd
tamilr0727-ux <tamilr0727@gmail.com>
2026-01-02 20:50:18 +0530

    Add files via upload

---
+++ Exercise_1/DataType.java
@@ -0,0 +1,13 @@
+public class DataType{
+    public static void main(String[] args){
+        char id='A';
+        String name="Rock Williams";
+        byte age=30;
+        short marks=1100;
+        int ph_num=687654321;
+        int[] sub_marks={67,23,98,90,89,70,98};
+        System.out.println("The Student with ID "+id+" and Name:"+name+",Age:"+age+"got Marks:"+marks);
+        System.out.println("Phone_no:"+ph_num);
+        System.out.println("Subject Marks:"+sub_marks);
+    }
+}
