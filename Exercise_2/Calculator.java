f5adbc6dfc28343f0c9c924b9a604c6644ad6d42
tamilr0727-ux <tamilr0727@gmail.com>
2026-01-02 20:58:08 +0530

    Add files via upload

---
+++ Exercise_2/Calculator.java
@@ -0,0 +1,15 @@
+public class Calculator{
+    public static void main(String[] args){
+        int n1=6;
+        int n2=9;
+        int a=n1+n2;
+        int b=n1-n2;
+        float c=n1*n2;
+        float d=n1/n2;
+        System.out.println("Addition of "+n1+" and "+n2+":"+a);
+        System.out.println("Subtraction of "+n1+" and "+n2+":"+b);
+        System.out.println("Multiplication of "+n1+" and "+n2+":"+c);
+        System.out.println("Division of "+n1+" and "+n2+":"+d);
+        
+    }
+}
